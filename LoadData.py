f691f44e8f989baa321f789b28b1ef792aad71ef
Bantouza <brianantony456@gmail.com>
2018-01-06 10:53:45 +0100

    Update LoadData.py

--- LoadData.py
+++ LoadData.py
@@ -1,25 +1,25 @@
 import sys
 
 xmlData1='''
-            <ns0:ID ns0:type="File_ID">WORKER_DOCUMENT-0-23432</ns0:ID>
+            <ns0:ID ns0:type="File_ID"></ns0:ID>
    '''
 
 xmlData2='''
             <!--Optional:-->
-            <ns0:ID>WORKER_DOCUMENT-0-23432</ns0:ID>
+            <ns0:ID>WORKER_DOCUMENT-0-33424234</ns0:ID>
             <ns0:Filename>abcd.docx</ns0:Filename>
             <ns0:Comment>Document Comment</ns0:Comment>
  
             <!--Optional:-->
-            <ns0:File>xkjkldjfoioojakjlsdjfklja</ns0:File>
+            <ns0:File>djaflkjaljfd</ns0:File>
             <ns0:Document_Category_Reference ns0:Descriptor="?">
                <!--Zero or more repetitions:-->
-               <ns0:ID ns0:type="WID">2380480328048230984098230948209388</ns0:ID>
+               <ns0:ID ns0:type="WID">2342342344663363</ns0:ID>
             </ns0:Document_Category_Reference>
 
             <ns0:Worker_Reference ns0:Descriptor="?">
                <!--Zero or more repetitions:-->
-               <ns0:ID ns0:type="Employee_ID">12345</ns0:ID>
+               <ns0:ID ns0:type="Employee_ID">3243242</ns0:ID>
             </ns0:Worker_Reference>
             <ns0:Content_Type>application/vnd.openxmlformats-officedocument.wordprocessingml.document</ns0:Content_Type>
 '''
